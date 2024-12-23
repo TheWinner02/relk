37d24d972862a42f057e70339fb14a62a6bb8c4d
TheWinner02 <dariovinci200217@gmail.com>
2024-12-23 12:20:14 +0100

    update relk source

--- app/src/main/java/com/relk/MainActivity.java
+++ app/src/main/java/com/relk/MainActivity.java
@@ -1,14 +1,20 @@
 package com.relk;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputLayout;
 import com.relk.ui.anime.AnimeFragment;
 import com.relk.ui.film.FilmFragment;
 import com.relk.ui.home.HomeFragment;
 import com.relk.ui.tv.TVFragment;
-
 import androidx.fragment.app.Fragment;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +25,27 @@ public class MainActivity extends AppCompatActivity {
 
         // Trova la BottomNavigationView dal layout
         BottomNavigationView bottomNavigationView = findViewById(R.id.nav_view);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        TextInputLayout searchBarLayout = findViewById(R.id.search_bar_layout);
+
+        // Aggiungi un click listener al FAB
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // All'interno del click listener del FAB
+                if (searchBarLayout.getVisibility() == View.GONE) {
+                    // Mostra la barra di ricerca con una transizione slide
+                    searchBarLayout.setVisibility(View.VISIBLE);
+                    Animation slideIn = AnimationUtils.loadAnimation(MainActivity.this, R.anim.slide_in_top);
+                    searchBarLayout.startAnimation(slideIn);
+                } else {
+                    // Nascondi la barra di ricerca con una transizione slide
+                    Animation slideOut = AnimationUtils.loadAnimation(MainActivity.this, R.anim.slide_out_top);
+                    searchBarLayout.startAnimation(slideOut);
+                    searchBarLayout.setVisibility(View.GONE);
+                }
+            }
+        });
 
         // Imposta la logica per ciascun elemento della barra di navigazione
         bottomNavigationView.setOnItemSelectedListener(item -> {
@@ -51,5 +78,6 @@ public class MainActivity extends AppCompatActivity {
         if (savedInstanceState == null) {
             bottomNavigationView.setSelectedItemId(R.id.home);  // Imposta il primo elemento come selezionato
         }
+
     }
 }
