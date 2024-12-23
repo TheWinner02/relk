1e43831ee5ffcbf145ca8e9e2b52e32cfe586cb9
TheWinner02 <dariovinci200217@gmail.com>
2024-12-23 10:51:03 +0100

    update relk source

--- app/src/main/java/com/relk/MainActivity.java
+++ app/src/main/java/com/relk/MainActivity.java
@@ -1,8 +1,6 @@
 package com.relk;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.relk.ui.anime.AnimeFragment;
@@ -23,33 +21,30 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = findViewById(R.id.nav_view);
 
         // Imposta la logica per ciascun elemento della barra di navigazione
-        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                // Dichiarazione del fragment da caricare
-                Fragment selectedFragment = null;
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            // Dichiarazione del fragment da caricare
+            Fragment selectedFragment;
 
-                // Gestisci la selezione dell'elemento nella barra di navigazione
-                int itemId = item.getItemId();
-                if (itemId == R.id.home) {// Sostituisci con il fragment per Home
-                    selectedFragment = new HomeFragment();
-                } else if (itemId == R.id.film) {// Sostituisci con il fragment per Search
-                    selectedFragment = new FilmFragment();
-                } else if (itemId == R.id.tv) {// Sostituisci con il fragment per Profile
-                    selectedFragment = new TVFragment();
-                } else if (itemId == R.id.anime) {// Sostituisci con il fragment per Profile
-                    selectedFragment = new AnimeFragment();
-                } else {
-                    return false;
-                }
-
-                // Sostituisci il fragment nel contenitore
-                getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.nav_host_fragment, selectedFragment) // Riferimento al contenitore del fragment
-                        .commit();
-
-                return true;
+            // Gestisci la selezione dell'elemento nella barra di navigazione
+            int itemId = item.getItemId();
+            if (itemId == R.id.home) {// Sostituisci con il fragment per Home
+                selectedFragment = new HomeFragment();
+            } else if (itemId == R.id.film) {// Sostituisci con il fragment per Search
+                selectedFragment = new FilmFragment();
+            } else if (itemId == R.id.tv) {// Sostituisci con il fragment per Profile
+                selectedFragment = new TVFragment();
+            } else if (itemId == R.id.anime) {// Sostituisci con il fragment per Profile
+                selectedFragment = new AnimeFragment();
+            } else {
+                return false;
             }
+
+            // Sostituisci il fragment nel contenitore
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.nav_host_fragment, selectedFragment) // Riferimento al contenitore del fragment
+                    .commit();
+
+            return true;
         });
 
         // Imposta il fragment iniziale, per esempio HomeFragment
